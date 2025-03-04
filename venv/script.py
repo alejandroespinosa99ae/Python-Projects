72568690915549ecaea67b4d5b6ba8434e48282c
alejandroespinosa99ae <153782195+alejandroespinosa99ae@users.noreply.github.com>
2025-03-03 19:09:48 -0600

    Update script.py
    The first version of the web scraping script file that retrieves all html text from the given URL

--- venv/script.py
+++ venv/script.py
@@ -2,5 +2,19 @@
 import httpx
 from selectolax.parser import HTMLParser
 
+url = "https://en.wikipedia.org/wiki/Lists_of_earthquakes#Deadliest_earthquakes"
+# To access the web
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0"}
+
+# Information that the website gives will be stored in the following variable
+resp = httpx.get(url, headers=headers)
+
+print(resp.text)
+
+
+
+
+
+
 
 
